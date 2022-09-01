f0dcc7595954f219086da7706c0a3cfbfc0e75ef
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2022-09-01 16:03:45 +0500

    Update purchase_approval.py

--- pways_purchase_approval/models/purchase_approval.py
+++ pways_purchase_approval/models/purchase_approval.py
@@ -4,12 +4,10 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError, ValidationError
 
 
-
 class PurchaseApproval(models.Model):
     _name = "purchase.approval"
     _description = "Purchase Approval"
     _rec_name = 'document_type'
-
 
     document_type = fields.Selection(
         [('purchase', 'Purchase Order')], string='Document Type', required=True)
@@ -18,18 +16,15 @@ class PurchaseApproval(models.Model):
     # custom field
     custom_vendor = fields.Many2one('res.partner', string="Vendor")
 
-   
+    # @api.constrains('approval_line_ids')
+    # def _check_duplicate_user_id(self):
+    #     for approval in self:
+    #         for user in approval.approval_line_ids.mapped("user_id"):
 
-    @api.constrains('approval_line_ids')
-    def _check_duplicate_user_id(self):
-        for approval in self:
-            for user in approval.approval_line_ids.mapped("user_id"):
-
-                line_ids = self.env['purchase.approval.lines'].search_count(
-                    [('approval_id.document_type', '=', 'purchase'), ('user_id', '=', user.id)])
-                if line_ids > 1:
-                    raise ValidationError(("Duplicated approver not allow"))
-
+    #             line_ids = self.env['purchase.approval.lines'].search_count(
+    #                 [('approval_id.document_type', '=', 'purchase'), ('user_id', '=', user.id)])
+    #             if line_ids > 1:
+    #                 raise ValidationError(("Duplicated approver not allow"))
 
 
 class PurchaseApprovalLines(models.Model):
