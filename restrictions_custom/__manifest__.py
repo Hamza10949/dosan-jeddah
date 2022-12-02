eb0372ecbf37875fe9f8da3835cbccbaf90dbe34
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2022-12-03 02:37:01 +0500

    prod_cat_code

--- restrictions_custom/__manifest__.py
+++ restrictions_custom/__manifest__.py
@@ -20,6 +20,8 @@
         'view/restrict.xml',
         'view/project_fields.xml',
         'view/inventory_custom_fields.xml',
+        'view/product_cus_chngs.xml',
+
 
 
     ],
--- restrictions_custom/models/__init__.py
+++ restrictions_custom/models/__init__.py
@@ -1,2 +1,3 @@
 from . import main_changes
 from . import inventory_custom
+from . import product_custom
