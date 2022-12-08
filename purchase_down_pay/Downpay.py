398f7cc1ed873580dc75c399c5a481cb6a854f58
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2022-12-08 13:39:45 +0500

    downpayment_amount_total_update

--- purchase_down_pay/Downpay.py
+++ purchase_down_pay/Downpay.py
@@ -10,11 +10,13 @@ class PurchaseOrder(models.Model):
 
     # picking_ids = fields.Many2many('account.payment', compute='_compute_payment_count', string='Payments')
     cout_payment = fields.Integer(compute='_compute_payment_count')
+    total_amount_pays = fields.Integer(compute='_total_payment_count')
 
     @api.depends('partner_id')
     def _compute_payment_count(self):
-        totalpay = self.env['account.payment'].search([('ref', '=', self.name)])
-        self.cout_payment=len(totalpay)
+        totalpay = self.env['account.payment'].search(
+            [('ref', '=', self.name)])
+        self.cout_payment = len(totalpay)
         # for rec in self:
         #     payment = self.env['account.payment'].search([('ref', '=', rec.name)])
         #     if payment:
@@ -25,43 +27,66 @@ class PurchaseOrder(models.Model):
         #     else:
         #         rec.cout_payment = 0
         #         rec.picking_ids = False
-    
+# custom changes by nadia
+
+    def _total_payment_count(self):
+        ven_bills = self.env['account.payment'].search([])
+        total = 0
+        for record in ven_bills:
+            for rec in self:
+                if rec.cout_payment > 1 and rec.name == record.ref:
+                    total += record.amount
+                else:
+                    self.total_amount_pays = 0
+        self.total_amount_pays = total
+
+    def compute_amount_field(self):
+        ven_bills = self.env['account.payment'].search([])
+        for record in ven_bills:
+            for rec in self:
+
+                # if rec.name==record.ref:
+                if rec.cout_payment == 0:
+                    return self.amount_total
+                if rec.cout_payment == 1 and rec.name == record.ref:
+                    sub = self.amount_total-record.amount
+                    return sub
+                if rec.cout_payment > 1 and rec.name == record.ref:
+                    return self.amount_total-self.total_amount_pays
+    # -------------------------------------------------
+
     def action_bill(self):
         return {
             'name': _('New Form'),
             'view_type': 'form',
             'view_mode': 'form',
             'res_model': 'account.payment',
-            'view_id': self.env.ref('account.view_account_payment_form').id,            
+            'view_id': self.env.ref('account.view_account_payment_form').id,
             'type': 'ir.actions.act_window',
             'context': {'default_partner_id': self.partner_id.id,
                         'default_partner_type': 'supplier',
-                        'default_ref':self.name,
-                        'default_payment_type':'outbound',
-                        'default_amount':self.amount_total}
+                        'default_ref': self.name,
+                        'default_payment_type': 'outbound',
+                        'default_amount': self.amount_total}
         }
 
     def action_payments(self):
-        
+
         action = self.env.ref('account.action_account_payments_payable')
-        
+
         result = {
-			'name': action.name,
-			'help': action.help,
-			'type': action.type,
-			'binding_view_types': action.binding_view_types,
-			'view_mode': action.view_mode,
-			'target': action.target,
-			'context': action.context,
-			'res_model': action.res_model,
-            'domain':[('ref','=',self.name)]
+            'name': action.name,
+            'help': action.help,
+            'type': action.type,
+            'binding_view_types': action.binding_view_types,
+            'view_mode': action.view_mode,
+            'target': action.target,
+            'context': action.context,
+            'res_model': action.res_model,
+            'domain': [('ref', '=', self.name)]
 
-		}
+        }
         return result
-
-
-
-
 
         # return {
         #     # 'domain': [('ref', 'in', self.name)],
@@ -69,24 +94,25 @@ class PurchaseOrder(models.Model):
         #     'view_type': 'tree',
         #     'view_mode': 'tree',
         #     'res_model': 'account.payment',
-        #     'view_id': self.env.ref('account.view_account_supplier_payment_tree').id, 
+        #     'view_id': self.env.ref('account.view_account_supplier_payment_tree').id,
         #     'type': 'ir.actions.act_window'
         # }
 
+
 class CreateInvoice(models.Model):
-    _name='purchase.down'
+    _name = 'purchase.down'
     name = fields.Char(string="Name")
     # @api.multi
     # def action_bill(self):
     #     raise UserError("Ata he")
-        # self.write({'state': 'in_payment'})
-        # '''return {
-        #     #'name': self.order_id,
-        #     'res_model': 'pickabite.payment',
-        #     'type': 'ir.actions.act_window',
-        #     'context': {},
-        #     'view_mode': 'form',
-        #     'view_type': 'form',
-        #     'view_id': self.env.ref("pickabite.payment_form_view"),
-        #     'target': 'new'
-        # } '''
+    # self.write({'state': 'in_payment'})
+    # '''return {
+    #     #'name': self.order_id,
+    #     'res_model': 'pickabite.payment',
+    #     'type': 'ir.actions.act_window',
+    #     'context': {},
+    #     'view_mode': 'form',
+    #     'view_type': 'form',
+    #     'view_id': self.env.ref("pickabite.payment_form_view"),
+    #     'target': 'new'
+    # } '''
