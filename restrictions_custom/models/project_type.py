44bd9706bd811a4c126331f2b4b176f00d6d7aa3
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-01-12 17:15:11 +0500

    Update project_type.py

--- restrictions_custom/models/project_type.py
+++ restrictions_custom/models/project_type.py
@@ -8,3 +8,9 @@ class PurchaseApproval(models.Model):
     _name = "project.type.custom"
 
     proj_type_name = fields.Char(string="Project Type")
+
+    def name_get(self):
+        result = []
+        for rec in self:
+            result.append((rec.id, rec.proj_type_name))
+        return result
